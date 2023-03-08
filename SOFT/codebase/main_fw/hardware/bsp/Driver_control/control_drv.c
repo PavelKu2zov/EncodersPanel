934714fc537fbe5b40fe18912565ba33c8cd1213
Pavel Kutuzov <p.kutuzov@yadro.com>
2023-03-08 20:01:37 +0300

    experement sw9

--- SOFT/codebase/main_fw/hardware/bsp/Driver_control/control_drv.c
+++ SOFT/codebase/main_fw/hardware/bsp/Driver_control/control_drv.c
@@ -461,6 +461,11 @@ static STD_RESULT update_sw9_input_prm(void)
 
     if (ENCODER_DIR_ROTATION_NONE != ENCODER_GET_DIR_ROTATION(encoder_impulses))
     {
+        table_t value;
+        value = a_sw10_fsm_table[sw10_input_prm.R];
+        create_midi_frame(value.prm.cc, value.prm.data);
+        USARTSend(bufferUartTx, MIDI_SIZE_FRAME);
+        
         sw9_input_prm.B.dir_rotation = ENCODER_GET_DIR_ROTATION(encoder_impulses);
 
         if (RESULT_OK == get_toggle_sw11_pos(&toggle_pos))
